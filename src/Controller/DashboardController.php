4a5454f7a3e33a507bfac015ac21a1428b9ebfdc
mdoens <michel.doens@sition.nl>
2026-02-24 22:35:08 +0100

    deploy: update

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -24,12 +24,15 @@ class DashboardController extends AbstractController
         $ibCash = $ibClient->getCashReport();
         $ibTotalValue = array_sum(array_column($ibPositions, 'value'));
         $ibTotalCost = array_sum(array_column($ibPositions, 'cost'));
+        $ibCashBalance = $ibCash['ending_cash'] ?? 0.0;
 
         // Saxo posities (null als niet ingelogd)
         $saxoPositions = null;
+        $saxoBalance = null;
         $saxoAuthenticated = $saxoClient->isAuthenticated();
         if ($saxoAuthenticated) {
             $saxoPositions = $saxoClient->getPositions();
+            $saxoBalance = $saxoClient->getAccountBalance();
             if ($saxoPositions === null) {
                 $saxoAuthenticated = false;
             }
@@ -37,22 +40,25 @@ class DashboardController extends AbstractController
 
         $saxoTotalPnl = $saxoPositions !== null ? array_sum(array_column($saxoPositions, 'pnl')) : 0;
         $saxoTotalExposure = $saxoPositions !== null ? array_sum(array_column($saxoPositions, 'exposure')) : 0;
+        $saxoCashBalance = (float) ($saxoBalance['CashBalance'] ?? 0);
 
         // Momentum signaal
         $signal = $momentumService->getSignal();
 
-        // Grand total
-        $grandTotal = $ibTotalValue + $saxoTotalExposure;
+        // Grand total (posities + cash)
+        $grandTotal = $ibTotalValue + $ibCashBalance + $saxoTotalExposure + $saxoCashBalance;
 
         return $this->render('dashboard/index.html.twig', [
             'ib_positions' => $ibPositions,
             'ib_cash' => $ibCash,
             'ib_total_value' => $ibTotalValue,
             'ib_total_cost' => $ibTotalCost,
+            'ib_cash_balance' => $ibCashBalance,
             'saxo_authenticated' => $saxoAuthenticated,
             'saxo_positions' => $saxoPositions,
             'saxo_total_pnl' => $saxoTotalPnl,
             'saxo_total_exposure' => $saxoTotalExposure,
+            'saxo_cash_balance' => $saxoCashBalance,
             'signal' => $signal,
             'grand_total' => $grandTotal,
         ]);
